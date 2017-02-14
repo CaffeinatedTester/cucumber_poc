035bab5366dfd2f581b24f3edb46ed2a490dcd98
David Skinner <David.Skinner@missguided.com>
2017-02-14 08:19:09 +0000

    update_unit_tests

--- features/step_definitions/unit_test_steps.rb
+++ features/step_definitions/unit_test_steps.rb
@@ -1,6 +1,6 @@
 Given(/^I pass a env parameter to the config$/) do
   config = FrameworkConfiguration.new
-  @region = 'fr'
+  @region = 'uk'
   @env = 'staging'
   config.set_region(@region)
   config.set_environment(@env)
@@ -8,11 +8,16 @@ end
 
 Then(/^then I will retrieve that env URI$/) do
   expect(@abstract.get_page_url).to eq(UrlManager.getFrontEnd)
+  expect(@abstract.get_page_url).to include(@env)
 end
 
 
 Given(/^I pass a region parameter to the config$/) do
-  pending # Write code here that turns the phrase above into concrete actions
+  config = FrameworkConfiguration.new
+  @region = 'fr'
+  @env = 'staging'
+  config.set_region(@region)
+  config.set_environment(@env)
 end
 
 
@@ -22,21 +27,19 @@ end
 
 
 Then(/^I can access that region URI$/) do
- # expect(@abstract.get_page_url).to == UrlManager.getFrontEnd
- # p @abstract.get_page_url
+  expect(@abstract.get_page_url).to eq(UrlManager.getFrontEnd)
+  expect(@abstract.get_page_url).to include(@region)
 end
 
 
 Given(/^I do not pass a parameter to the config$/) do
-  pending # Write code here that turns the phrase above into concrete actions
+  config = FrameworkConfiguration.new
+  config.set_region
+  config.set_environment
 end
-
-
-When(/^I create a new gloval map object$/) do
-  pending # Write code here that turns the phrase above into concrete actions
-end
-
 
 Then(/^the URI will return the defaul value$/) do
-  pending # Write code here that turns the phrase above into concrete actions
+  expect(@abstract.get_page_url).to eq(UrlManager.getFrontEnd)
+  expect(@abstract.get_page_url).to include('uk')
+  expect(@abstract.get_page_url).to include('staging')
 end
